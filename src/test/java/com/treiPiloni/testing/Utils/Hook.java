a57deb985ce9f173f69779e3eef58b85c4cb3344
Vlad Iliescu <vlad.iliescu@3pillarglobal.com>
2017-06-08 13:36:29 +0300

    zzz

--- src/test/java/com/treiPiloni/testing/Utils/Hook.java
+++ src/test/java/com/treiPiloni/testing/Utils/Hook.java
@@ -28,7 +28,7 @@ public class Hook {
 
         DesiredCapabilities capability = DesiredCapabilities.chrome();
         try {
-            baseUtil.driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), capability);
+            baseUtil.driver = new RemoteWebDriver(new URL("http://486:8080:4444/wd/hub"), capability);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
