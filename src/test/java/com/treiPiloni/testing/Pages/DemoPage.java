5463794a65bdd84bac4925b89af811841d3124c3
Vlad Iliescu <vlad.iliescu@3pillarglobal.com>
2017-06-08 15:56:13 +0300

    zzz

--- src/test/java/com/treiPiloni/testing/Pages/DemoPage.java
+++ src/test/java/com/treiPiloni/testing/Pages/DemoPage.java
@@ -1,15 +1,21 @@
 package com.treiPiloni.testing.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by vlad.iliescu on 06/06/2017.
  */
 public class DemoPage {
+    private WebDriver driver;
+
     public DemoPage(WebDriver driver) {
+        this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
@@ -24,6 +30,8 @@ public class DemoPage {
     }
 
     public void enterIntoSearchField(String firstKeyWord, String secondKeyWord) {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        searchIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='nav-search-icon']")));
         searchField.click();
         searchField.sendKeys(firstKeyWord + "" + secondKeyWord);
         //wait a bit to visually see results
